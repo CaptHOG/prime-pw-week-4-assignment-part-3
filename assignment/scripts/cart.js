c0f7dc626975582b4ff73c51bb610df180cfec53
Jeremy Hanson <rockbasser88@hotmail.com>
2022-11-17 11:58:47 -0600

    added addItem function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,15 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = [];
+
+function addItem(item) {
+    basket.push(item);
+    console.log(`Item added: ${item}`);
+    return true;
+}
+
+console.log(addItem(1));
+console.log(addItem(2));
+console.log(addItem(3));
