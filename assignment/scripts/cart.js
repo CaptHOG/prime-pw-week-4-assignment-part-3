d97e57bb1876c357985803b57880399a76eec63f
Jeremy Hanson <rockbasser88@hotmail.com>
2022-11-18 15:39:07 -0600

    added isFull function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -4,6 +4,7 @@ console.log('***** Cart Functions *****');
 
 // establish basket to put items into
 let basket = [];
+const maxItems = 5;
 console.log('Ready to buy groceries. Basket is', + basket);
 console.log(basket);
 function addItem(item) {
@@ -15,7 +16,7 @@ console.log(`Apples added (expect true) ${addItem('apples')}`);
 console.log(`Bread added (expect true) ${addItem('bread')}`);
 console.log(`Candy added (expect true) ${addItem('candy')}`);
 console.log(`Drinks added (expect true) ${addItem('drinks')}`);
-console.log(`Basket is now ${basket}`);
+console.log('Basket is now:', basket);
 // end addItem
 
 
@@ -27,6 +28,17 @@ function listItems() {
 }
 console.log('These items are in the basket:');
 listItems();
+
+
+//checking if basket is full
+function isFull() {
+    if (basket.length < maxItems) {
+        return false;
+    } else {
+        return true;
+    }
+}
+console.log(isFull());
 
 
 // empties basket
