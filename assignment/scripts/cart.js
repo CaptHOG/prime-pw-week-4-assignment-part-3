8e9d10f7a8b7734dc87b06f8e8455d69a60007bc
Jeremy Hanson <rockbasser88@hotmail.com>
2022-11-18 17:05:17 -0600

    added isFull to addItem

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,22 +2,35 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+
 // establish basket to put items into
 let basket = [];
 const maxItems = 5;
-console.log('Ready to buy groceries. Basket is', + basket);
-console.log(basket);
+console.log('Ready to buy groceries. Basket is empty:', + basket);
+// just playing with concatenation and logging; learning syntax differences
+console.log('Basket is', basket);
+console.log(`Basket can hold ${maxItems} items.`);
+
+
+// adding items to basket
 function addItem(item) {
-    basket.push(item);
-    console.log(`Item added: ${item}`);
-    return true;
+    if (isFull() === false) {
+        basket.push(item);
+        console.log(`Item added: ${item}`);
+        return true;
+    } else {
+        return false;
+    }
+    
 }
 console.log(`Apples added (expect true) ${addItem('apples')}`);
 console.log(`Bread added (expect true) ${addItem('bread')}`);
 console.log(`Candy added (expect true) ${addItem('candy')}`);
 console.log(`Drinks added (expect true) ${addItem('drinks')}`);
 console.log('Basket is now:', basket);
-// end addItem
+console.log(`Basket is full: ${isFull()}`);
+console.log(addItem('eggs'));
+console.log('Basket is now:', basket);
 
 
 // listing items in basket
@@ -38,7 +51,7 @@ function isFull() {
         return true;
     }
 }
-console.log(isFull());
+console.log(`Basket is full: ${isFull()}`);
 
 
 // empties basket
